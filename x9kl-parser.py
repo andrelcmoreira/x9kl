70132339511f3d4ed448cccefd5743d485d07516
André L. C. Moreira <andrelcmoreira@disroot.org>
2023-06-28 00:29:19 -0300

    adding support to more keys

--- x9kl-parser.py
+++ x9kl-parser.py
@@ -2,6 +2,7 @@ from argparse import ArgumentParser
 from sys import argv
 from enum import Enum
 
+# https://github.com/torvalds/linux/blob/master/include/uapi/linux/input-event-codes.h
 class KeyCode(Enum):
     KEY_0 = 11
     KEY_1 = 2
@@ -16,7 +17,7 @@ class KeyCode(Enum):
     KEY_A = 30
     KEY_B = 48
     KEY_C = 46
-    KEY_D = 42
+    KEY_D = 32
     KEY_E = 18
     KEY_F = 33
     KEY_G = 34
@@ -42,60 +43,72 @@ class KeyCode(Enum):
     KEY_ENTER = 28
     KEY_SPACE = 57
     KEY_MINUS = 12
+    KEY_COMMA = 51
+    KEY_DOT = 52
+    KEY_SEMICOLON = 39
+    KEY_EQUAL = 117
+    KEY_RO = 89
+    KEY_GRAVE = 41
+    KEY_102ND = 86
+    KEY_RIGHTBRACE = 27
+    KEY_BACKSLASH = 43
 
 
-#'''
-#{KEY_COMMA, {',', '<', '\0', handle_key}},
-#{KEY_DOT, {'.', '>', '\0', handle_key}},
-#{KEY_MINUS, {'-', '_', '\0', handle_key}},
-#{KEY_SEMICOLON, {';', ':', '\0', handle_key}},
-#{KEY_EQUAL, {'=', '+', '\0', handle_key}},
-#{KEY_SPACE, {' ', '\0', '\0', handle_key}},
-#{KEY_ENTER, {'\0', '\0', '\0', handle_enter}},
-#{KEY_BACKSPACE, {'\0', '\0', '\0', handle_backspace}},
-#{KEY_CAPSLOCK, {'\0', '\0', '\0', handle_capslock}},
-#{KEY_DELETE, {'\0', '\0', '\0', handle_delete}},
-#{KEY_RIGHT, {'\0', '\0', '\0', handle_arrow}},
-#{KEY_LEFT, {'\0', '\0', '\0', handle_arrow}},
-#{KEY_LEFTSHIFT, {'\0', '\0', '\0', handle_shift}},
-#{KEY_RO, {'/', '?', '\0', handle_key}},
-#{KEY_GRAVE, {'\'', '\"', '\0', handle_key}},
-#{KEY_102ND, {'\\', '|', '\0', handle_key}},
-#{KEY_RIGHTBRACE, {'[', '{', '\0', handle_key}},
-#{KEY_RIGHTALT, {'\0', '\0', '\0', handle_altgr}},
-#{KEY_BACKSLASH, {']', '}', '\0', handle_key}}};
-#'''
+# 'n' = normal
+# 's' = shift
+# 'c' = capslock
+# 'a' = altgr
 KEYMAP = {
-    KeyCode.KEY_A.value: { 'norm': 'a', 'shi': 'A', 'caps': 'A', 'alt': '' },
-    KeyCode.KEY_B.value: { 'norm': 'b', 'shi': 'B', 'caps': 'B', 'alt': '' },
-    KeyCode.KEY_C.value: { 'norm': 'c', 'shi': 'C', 'caps': 'C', 'alt': '' },
-    KeyCode.KEY_D.value: { 'norm': 'd', 'shi': 'D', 'caps': 'D', 'alt': '' },
-    KeyCode.KEY_E.value: { 'norm': 'e', 'shi': 'E', 'caps': 'E', 'alt': '' },
-    KeyCode.KEY_F.value: { 'norm': 'f', 'shi': 'F', 'caps': 'F', 'alt': '' },
-    KeyCode.KEY_G.value: { 'norm': 'g', 'shi': 'G', 'caps': 'G', 'alt': '' },
-    KeyCode.KEY_H.value: { 'norm': 'h', 'shi': 'H', 'caps': 'H', 'alt': '' },
-    KeyCode.KEY_I.value: { 'norm': 'i', 'shi': 'I', 'caps': 'I', 'alt': '' },
-    KeyCode.KEY_J.value: { 'norm': 'j', 'shi': 'J', 'caps': 'J', 'alt': '' },
-    KeyCode.KEY_K.value: { 'norm': 'k', 'shi': 'K', 'caps': 'K', 'alt': '' },
-    KeyCode.KEY_L.value: { 'norm': 'l', 'shi': 'L', 'caps': 'L', 'alt': '' },
-    KeyCode.KEY_M.value: { 'norm': 'm', 'shi': 'M', 'caps': 'M', 'alt': '' },
-    KeyCode.KEY_N.value: { 'norm': 'n', 'shi': 'N', 'caps': 'N', 'alt': '' },
-    KeyCode.KEY_O.value: { 'norm': 'o', 'shi': 'O', 'caps': 'O', 'alt': '' },
-    KeyCode.KEY_P.value: { 'norm': 'p', 'shi': 'P', 'caps': 'P', 'alt': '' },
-    KeyCode.KEY_Q.value: { 'norm': 'q', 'shi': 'Q', 'caps': 'Q', 'alt': '' },
-    KeyCode.KEY_R.value: { 'norm': 'r', 'shi': 'R', 'caps': 'R', 'alt': '' },
-    KeyCode.KEY_S.value: { 'norm': 's', 'shi': 'S', 'caps': 'S', 'alt': '' },
-    KeyCode.KEY_T.value: { 'norm': 't', 'shi': 'T', 'caps': 'T', 'alt': '' },
-    KeyCode.KEY_U.value: { 'norm': 'u', 'shi': 'U', 'caps': 'U', 'alt': '' },
-    KeyCode.KEY_V.value: { 'norm': 'v', 'shi': 'V', 'caps': 'V', 'alt': '' },
-    KeyCode.KEY_W.value: { 'norm': 'w', 'shi': 'W', 'caps': 'W', 'alt': '' },
-    KeyCode.KEY_X.value: { 'norm': 'x', 'shi': 'X', 'caps': 'X', 'alt': '' },
-    KeyCode.KEY_Y.value: { 'norm': 'y', 'shi': 'Y', 'caps': 'Y', 'alt': '' },
-    KeyCode.KEY_Z.value: { 'norm': 'H', 'shi': 'Z', 'caps': 'Z', 'alt': '' },
-    KeyCode.KEY_SPACE.value: { 'norm': ' ', 'shi': ' ', 'caps': ' ', 'alt': '' },
-    KeyCode.KEY_MINUS.value: { 'norm': '-', 'shi': '_', 'caps': '-', 'alt': '' },
-    KeyCode.KEY_ENTER.value: { 'norm': '\n', 'shi': '\n', 'caps': '\n', 'alt': '\n'}
+    KeyCode.KEY_0.value: { 'n': '0', 's': ')', 'c': '0', 'a': '' },
+    KeyCode.KEY_1.value: { 'n': '1', 's': '!', 'c': '1', 'a': '' },
+    KeyCode.KEY_2.value: { 'n': '2', 's': '@', 'c': '2', 'a': '' },
+    KeyCode.KEY_3.value: { 'n': '3', 's': '#', 'c': '3', 'a': '' },
+    KeyCode.KEY_4.value: { 'n': '4', 's': '$', 'c': '4', 'a': '' },
+    KeyCode.KEY_5.value: { 'n': '5', 's': '%', 'c': '5', 'a': '' },
+    KeyCode.KEY_6.value: { 'n': '6', 's': ' ', 'c': '6', 'a': '' },
+    KeyCode.KEY_7.value: { 'n': '7', 's': '&', 'c': '7', 'a': '' },
+    KeyCode.KEY_8.value: { 'n': '8', 's': '*', 'c': '8', 'a': '' },
+    KeyCode.KEY_9.value: { 'n': '9', 's': '(', 'c': '9', 'a': '' },
+    KeyCode.KEY_A.value: { 'n': 'a', 's': 'A', 'c': 'A', 'a': '' },
+    KeyCode.KEY_B.value: { 'n': 'b', 's': 'B', 'c': 'B', 'a': '' },
+    KeyCode.KEY_C.value: { 'n': 'c', 's': 'C', 'c': 'C', 'a': '' },
+    KeyCode.KEY_D.value: { 'n': 'd', 's': 'D', 'c': 'D', 'a': '' },
+    KeyCode.KEY_E.value: { 'n': 'e', 's': 'E', 'c': 'E', 'a': '' },
+    KeyCode.KEY_F.value: { 'n': 'f', 's': 'F', 'c': 'F', 'a': '' },
+    KeyCode.KEY_G.value: { 'n': 'g', 's': 'G', 'c': 'G', 'a': '' },
+    KeyCode.KEY_H.value: { 'n': 'h', 's': 'H', 'c': 'H', 'a': '' },
+    KeyCode.KEY_I.value: { 'n': 'i', 's': 'I', 'c': 'I', 'a': '' },
+    KeyCode.KEY_J.value: { 'n': 'j', 's': 'J', 'c': 'J', 'a': '' },
+    KeyCode.KEY_K.value: { 'n': 'k', 's': 'K', 'c': 'K', 'a': '' },
+    KeyCode.KEY_L.value: { 'n': 'l', 's': 'L', 'c': 'L', 'a': '' },
+    KeyCode.KEY_M.value: { 'n': 'm', 's': 'M', 'c': 'M', 'a': '' },
+    KeyCode.KEY_N.value: { 'n': 'n', 's': 'N', 'c': 'N', 'a': '' },
+    KeyCode.KEY_O.value: { 'n': 'o', 's': 'O', 'c': 'O', 'a': '' },
+    KeyCode.KEY_P.value: { 'n': 'p', 's': 'P', 'c': 'P', 'a': '' },
+    KeyCode.KEY_Q.value: { 'n': 'q', 's': 'Q', 'c': 'Q', 'a': '/' },
+    KeyCode.KEY_R.value: { 'n': 'r', 's': 'R', 'c': 'R', 'a': '' },
+    KeyCode.KEY_S.value: { 'n': 's', 's': 'S', 'c': 'S', 'a': '' },
+    KeyCode.KEY_T.value: { 'n': 't', 's': 'T', 'c': 'T', 'a': '' },
+    KeyCode.KEY_U.value: { 'n': 'u', 's': 'U', 'c': 'U', 'a': '' },
+    KeyCode.KEY_V.value: { 'n': 'v', 's': 'V', 'c': 'V', 'a': '' },
+    KeyCode.KEY_W.value: { 'n': 'w', 's': 'W', 'c': 'W', 'a': '?' },
+    KeyCode.KEY_X.value: { 'n': 'x', 's': 'X', 'c': 'X', 'a': '' },
+    KeyCode.KEY_Y.value: { 'n': 'y', 's': 'Y', 'c': 'Y', 'a': '' },
+    KeyCode.KEY_Z.value: { 'n': 'H', 's': 'Z', 'c': 'Z', 'a': '' },
+    KeyCode.KEY_SPACE.value: { 'n': ' ', 's': ' ', 'c': ' ', 'a': '' },
+    KeyCode.KEY_MINUS.value: { 'n': '-', 's': '_', 'c': '-', 'a': '' },
+    KeyCode.KEY_ENTER.value: { 'n': '\n', 's': '\n', 'c': '\n', 'a': '\n'},
+    KeyCode.KEY_COMMA.value: { 'n': ',', 's': '<', 'c': ',', 'a': '' },
+    KeyCode.KEY_DOT.value: { 'n': '.', 's': '>', 'c': '.', 'a': '' },
+    KeyCode.KEY_SEMICOLON.value: { 'n': ';', 's': ':', 'c': ';', 'a': '' },
+    KeyCode.KEY_EQUAL.value: { 'n': '=', 's': '+', 'c': '=', 'a': '' },
+    KeyCode.KEY_RO.value: { 'n': '/', 's': '?', 'c': '/', 'a': '' },
+    KeyCode.KEY_GRAVE.value: { 'n': '\'', 's': '"', 'c': '\'', 'a': '' },
+    KeyCode.KEY_102ND.value: { 'n': '\\', 's': '|', 'c': '\\', 'a': '' },
+    KeyCode.KEY_RIGHTBRACE.value: { 'n': '[', 's': '{', 'c': '[', 'a': '' },
+    KeyCode.KEY_BACKSLASH.value: { 'n': ']', 's': '}', 'c': ']', 'a': '' }
 }
+
 
 CAPS_MASK = 0x01
 SHIFT_MASK = 0x02
@@ -120,10 +133,10 @@ class LogEntry:
                 shift_pressed = True if (self.keys[i] & SHIFT_MASK) else False
                 altgr_pressed = True if (self.keys[i] & ALTGR_MASK) else False
             else:
-                if shift_pressed: s += KEYMAP[self.keys[i]]['shi']
-                elif caps_pressed: s += KEYMAP[self.keys[i]]['caps']
-                elif altgr_pressed: s += KEYMAP[self.keys[i]]['alt']
-                else: s += KEYMAP[self.keys[i]]['norm']
+                if shift_pressed: s += KEYMAP[self.keys[i]]['s']
+                elif caps_pressed: s += KEYMAP[self.keys[i]]['c']
+                elif altgr_pressed: s += KEYMAP[self.keys[i]]['a']
+                else: s += KEYMAP[self.keys[i]]['n']
 
         return s
 
